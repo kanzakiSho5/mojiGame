c337c4d1762e2194ed69a1352e03fd54bc27d3d2
kanzakiSho5 <syo5.4545@gmail.com>
2019-04-30 20:09:26 +0900

    モックアップの作成 - blockの作成   - ブロックの落下、Positionの設定、文字をランダムに切り替える機構の作成 - 落下時間、固定時間、移動機能の追加

---
+++ mojiGame/Assets/Scripts/Cube.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private char blockChar;
+    [SerializeField]
+    public bool isMovable;
+    [SerializeField]
+    public Vector2Int Pos;
+    
+    private void OnEnable()
+    {
+        Pos = new Vector2Int(4, 0);
+        isMovable = true;
+        SetChar(true);
+    }
+
+    private void Update()
+    {
+        // TODO: いずれ操作系を専用のManagerに移行
+        
+
+        gameObject.transform.position = new Vector3(Pos.x - 1, (20 - Pos.y) - 1, 0);
+    }
+
+    /// <summary>
+    /// ブロックの文字を設定
+    /// </summary>
+    /// <param name="DropItem">trueで"メメタァクパパン"のみ選ばれる</param>
+    public void SetChar(bool DropItem = false)
+    {
+        if(DropItem)
+        {
+            int rand = Random.Range(0, 8);
+            char ret;
+            switch(rand)
+            {
+                case 0:
+                    ret = 'め';
+                    break;
+                case 1:
+                    ret = 'め';
+                    break;
+                case 2:
+                    ret = 'た';
+                    break;
+                case 3:
+                    ret = 'ぁ';
+                    break;
+                case 4:
+                    ret = 'く';
+                    break;
+                case 5:
+                    ret = 'ぱ';
+                    break;
+                case 6:
+                    ret = 'ぱ';
+                    break;
+                case 7:
+                    ret = 'ん';
+                    break;
+                default:
+                    ret = 'め';
+                    break;
+            }
+
+            blockChar = ret;
+        }
+        else
+        {
+            int rand = Random.Range(12353, 12436);
+            blockChar = (char)rand;
+        }
+
+        if (text != null)
+            text.SetText(blockChar.ToString());
+    }
+
+    /// <summary>
+    /// 1ブロック分下げる
+    /// </summary>
+    public void DropBlock()
+    {
+        Pos.y++;
+    }
+}
---
+++ mojiGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    int Score;
+    [Header("Blocks Stetus")]
+    [SerializeField]
+    GameObject blockPrefab;
+    [SerializeField]
+    float blockDownSpeed = 2f;                      // ブロックの落ちるスピード
+    [SerializeField]
+    float blockFixSpeed = .5f;                      // ブロックが固定される時間
+
+    [Header("Debug")]
+    public float currentTime;
+    private int[,] fieldStatus = new int[21,8];     // フィールドにブロックが置かれているかどうか 0:なし 1:あり
+    [SerializeField]
+    private float currentFixTime;
+    [SerializeField]
+    private float currentDownTime;
+    private Cube currentMovableCube;
+
+
+    private void Awake()
+    {
+        init();
+    }
+
+    private void Update()
+    {
+        MoveBlock();
+
+        if(currentFixTime > blockFixSpeed)
+        {
+            currentFixTime = 0f;
+            currentMovableCube.isMovable = false;
+            fieldStatus[currentMovableCube.Pos.y, currentMovableCube.Pos.x] = 1;
+            PrintField();
+            currentMovableCube = Instantiate<GameObject>(blockPrefab).GetComponent<Cube>();
+        }
+
+        if(currentDownTime > blockDownSpeed && isCanMoveBlock())
+        {
+            currentDownTime = 0f;
+            currentMovableCube.DropBlock();
+        }
+
+        UpdateTime();
+    }
+
+    private void MoveBlock()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && isCanMoveBlock(-1, 0))
+        {
+            currentMovableCube.Pos.x--;
+            currentFixTime = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && isCanMoveBlock(1, 0))
+        {
+            currentMovableCube.Pos.x++;
+            currentFixTime = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && isCanMoveBlock())
+        {
+            currentMovableCube.Pos.y++;
+            currentDownTime = 0f;
+        }
+    }
+
+    
+    private bool isCanMoveBlock(int Horizontal = 0, int Vertical = 1)
+    {
+        if(fieldStatus[currentMovableCube.Pos.y + Vertical, currentMovableCube.Pos.x + Horizontal] == 0)
+            return true;
+        return false;
+    }
+
+
+
+    private void UpdateTime()
+    {
+        currentDownTime += Time.deltaTime;
+        currentTime     += Time.deltaTime;
+
+        if (!isCanMoveBlock())
+            currentFixTime += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void init()
+    {
+        for (int i = 0; i < fieldStatus.GetLength(0); i++)
+        {
+            for (int j = 0; j < fieldStatus.GetLength(1); j++)
+            {
+                fieldStatus[i, j] = 0;
+                if (i == 20 || j == 0 || j == 7)
+                    fieldStatus[i, j] = 1;
+            }
+        }
+
+        currentMovableCube = Instantiate<GameObject>(blockPrefab).GetComponent<Cube>();
+        currentTime = 0;
+        currentFixTime = 0;
+        currentDownTime = 0;
+
+        PrintField();
+    }
+
+    private void PrintField()
+    {
+        string DebStr = "";
+        for (int i = 0; i < fieldStatus.GetLength(0); i++)
+        {
+            for (int j = 0; j < fieldStatus.GetLength(1); j++)
+            {
+                DebStr += fieldStatus[i, j].ToString();
+            }
+            DebStr += "\n";
+        }
+        Debug.Log(DebStr);
+    }
+}
