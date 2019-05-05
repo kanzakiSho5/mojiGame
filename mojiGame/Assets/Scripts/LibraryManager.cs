cc558d8bcd703bf936e59d8d3aaa55c0a0384d33
kanzakiSho5 <syo5.4545@gmail.com>
2019-05-05 15:19:08 +0900

    辞書ファイルの読み込み機能の作成

---
+++ mojiGame/Assets/Scripts/LibraryManager.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraryManager : MonoBehaviour
+{
+
+    Library library;
+
+    private void Awake()
+    {
+        using (var reader = new StreamReader(Application.streamingAssetsPath + "/Library.json", System.Text.Encoding.UTF8))
+        {
+            string JsonStr = reader.ReadToEnd();
+            Debug.Log(JsonStr);
+        }
+    }
+}
+
+[System.Serializable]
+public class Library
+{
+    public string word;
+    public string mean;
+
+    /// <summary>
+    /// JsonをLbrary形式で返します。
+    /// </summary>
+    /// <param name="json">string型のjson文字列</param>
+    /// <returns>Library</returns>
+    public static Library CreateFromJson(string json)
+    {
+        return JsonUtility.FromJson<Library>(json);
+    }
+}
