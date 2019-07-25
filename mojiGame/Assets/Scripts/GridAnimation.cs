04958b7387210e37e555cf6188874bc92f1b0b87
kanzakiSho5 <syo5.4545@gmail.com>
2019-07-26 00:28:18 +0900

    ゲームシーンにGridを表示するアニメーションの作成

---
+++ mojiGame/Assets/Scripts/GridAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] GridSprites;
+    [SerializeField]
+    private Image image;
+
+    private void OnEnable()
+    {
+        StartCoroutine(AnimateGrid());
+    }
+
+    private IEnumerator AnimateGrid()
+    {
+        for(int i = 0; i < GridSprites.Length; i++)
+        {
+            image.sprite = GridSprites[i];
+            yield return new WaitForSeconds(0.06f);
+        }
+    }
+}
